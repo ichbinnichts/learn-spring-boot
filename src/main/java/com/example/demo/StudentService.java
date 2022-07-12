992ee45e70ca017d35e61a18602cf0141d7542b0
Nathan Víctor de Faria <nathan.faria@inatel.br>
2022-07-12 09:38:42 -0300

    feat: configure method addNewStudent to add to database

--- src/main/java/com/example/demo/StudentService.java
+++ src/main/java/com/example/demo/StudentService.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,15 +9,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class StudentService {
 
-    private final StudentRepository studentRepository;
+    private static StudentRepository studentRepository;
     @Autowired
     public StudentService(StudentRepository studentRepository) {
-        this.studentRepository = studentRepository;
+        StudentService.studentRepository = studentRepository;
     }
     public List<Student> getStudents(){
         return studentRepository.findAll();
     }
     public static void addNewStudent(Student student) {
-        System.out.println(student);
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
     }
 }
