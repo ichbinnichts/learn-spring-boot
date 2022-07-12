d09033308b07dc73cfa5203d31f8e1495346383c
Nathan Víctor de Faria <nathan.faria@inatel.br>
2022-07-12 10:09:12 -0300

    feat: add deleteStudent method

--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +28,9 @@ public class StudentController {
     @PostMapping
     public void registerNewStudent(@RequestBody Student student){
         StudentService.addNewStudent(student);
+    }
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long StudentId){
+        studentService.deleteStudent(StudentId);
     }
 }
